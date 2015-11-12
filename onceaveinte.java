f867a372a7a8243aec81bc8dbc69a561ca59e468
franmac23 <franmacdeveloper@gmail.com>
2015-11-12 18:04:52 +0100

    Revert "Subida inicial de tres archivos"
    This reverts commit 3ce34882845c671d062743ce3b81926c64e2be28.

--- onceaveinte.java
+++
@@ -1,6 +0,0 @@
- public static void main(String[] args) {
-
-	for (int i=11;i<21;i++){
- 		System.out.println(i);
-	}
-}
--- ventiunoatreinta.java
+++
@@ -1,6 +0,0 @@
- public static void main(String[] args) {
-
-	for (int i=21;i<31;i++){
- 		System.out.println(i);
-	}
-}
