3ce34882845c671d062743ce3b81926c64e2be28
franmac23 <franmacdeveloper@gmail.com>
2015-11-12 17:55:56 +0100

    Subida inicial de tres archivos
    Se suben tres archivos con extension .java para que cuentan de 1 a 10 , de 11 a 20 y de 21 a 30 respectivamente

---
+++ onceaveinte.java
@@ -0,0 +1,6 @@
+ public static void main(String[] args) {
+
+	for (int i=11;i<21;i++){
+ 		System.out.println(i);
+	}
+}
---
+++ ventiunoatreinta.java
@@ -0,0 +1,6 @@
+ public static void main(String[] args) {
+
+	for (int i=21;i<31;i++){
+ 		System.out.println(i);
+	}
+}
